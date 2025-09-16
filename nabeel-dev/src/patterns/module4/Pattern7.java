2b58c6dbd32f59cb21ef5967c22cd03357941326
nabeel.h <iamnabeelhashim@gmail.com>
2025-09-16 15:28:38 +0400

    lecture 4 completed

---
+++ nabeel-dev/src/patterns/module4/Pattern7.java
@@ -0,0 +1,24 @@
+package patterns.module4;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < i + 1; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
---
+++ nabeel-dev/src/patterns/module4/Pattern8.java
@@ -0,0 +1,21 @@
+package patterns.module4;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
