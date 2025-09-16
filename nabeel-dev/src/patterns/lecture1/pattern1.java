4784232d4e5ff845a175bb01cfb691d7fa358e48
nabeel.h <iamnabeelhashim@gmail.com>
2025-09-16 11:30:34 +0400

    lecture 1

---
+++ nabeel-dev/src/patterns/lecture1/pattern1.java
@@ -0,0 +1,16 @@
+package patterns.lecture1;
+
+public class pattern1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print(0);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
